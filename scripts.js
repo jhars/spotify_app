d6605b518cd79122a237ee04283618d27aef1672
Jon Harlan <jonharlan@Jons-MacBook-Pro-2.local>
2015-07-07 09:09:40 -0700

    AM commit

--- scripts.js
+++ scripts.js
@@ -1,34 +1,73 @@
 $(function(){
 
-//-------------CONSTRUCTOR FOR SONG+ARTIST-------//
+//-------------CONSTRUCTOR FOR SONG+ARTIST == NO NEED-------//
+var templateData = {
+            albumArt: result.album.images.length > 0 ? result.album.images[0].url : null,
+            artist: result.artists[0].name,
+            name: result.name,
+            previewUrl: result.preview_url
+          };
 
-var SearchReturn = function (song, artist){
-	this.song = song;
-	this.artist = artist;
+ // var templateData = function (song,artist,art,link){
+ // 	this.song =  
+
+ // });
+
+
+SearchTerms.prototype.render = function(){
+	var $song_item = $(songTemplate(this));
+	this.index = SearchTerms.all.indexOf(this);
+	$song_item.attr('data-index',this.index);
+	$resultsList.append($song_item);
+
+	//this is where I should iterate through array
+	// spot_search["tracks"]["items"][1]["name"];
 }
 
-SearchReturn.all = [];
-
-SearchReturn.prototype.save = function(){
-	SearchReturn.all.push(this);
-}
-
-SearchReturn.prototype.render = function(){
-	var 
-}
+var $spotifySearch = $('#spot_search');
+var $newSearch= $('#track');
+var $resultsList=$('#results');
+var songTemplate = _.template($('#song-template').html());
 
 
-$('form').on('submit', function(){
-	var $track_search = $('#track')
 
-  $.get('https://api.spotify.com/v1/search?
-  	q='+$track_search, function(data) {
-    console.log(data);
-  });
+
+
+$spotifySearch.on('submit', function(event){
+	event.preventDefault();
+
+	var track_search = $newSearch.val();
+	var searchAPI = 'https://api.spotify.com/v1/search?
+  	q='+track_search;
+
+  $.get(searchAPI, function(data) {
+  	var searchResults = data.tracks.items;
+    console.log(searchResults);
+
+    if (searchResults.length > 0) {
+    	_.each(searchResults, function(result, index){
+
+    		var var templateData = {
+            albumArt: result.album.images.length > 0 ? result.album.images[0].url : null,
+            artist: result.artists[0].name,
+            name: result.name,
+            previewUrl: result.preview_url
+          };
+// would like to put this into constructor
+		  var $trackResult = $(trackTemplate(templateData));
+		  $results.append($trackResult);
+		});
+
+    	  } else {
+    	    $results.append('No results.');
+    	  }
+    	});
+		  $spotifySearch[0].reset();
+		  $track.focus();
+    	});
 });
 
 
-// spot_search["tracks"]["items"][1]["name"];
 
 
 
